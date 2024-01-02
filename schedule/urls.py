c61e75828c7d8c0b0a1b4cee6f6a971b44ff252f
hyeoncheol Kim <user.name>
2024-01-02 13:26:01 +0900

    병합 전 변경사항 커밋

--- schedule/urls.py
+++ schedule/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.GetUserEventsView.as_view(), name='get_user_events'),
+    path('events/', views.GetUserEventsView.as_view(), name='get_user_events'),
     path('create/', views.CreateEventView.as_view(), name='create_event'),
 ]
